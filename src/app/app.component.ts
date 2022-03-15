81b54e405e3a4b4c8fa555988b8079f90a5c1df1
Novikov Ivan <ivan.novikov.1990@list.ru>
2022-03-15 12:41:45 +0300

    init heroes service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'get-started-app';
 }
